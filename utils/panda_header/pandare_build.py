f04d26b299bf3b72e3db4ec7b11f97bebf684613
Luke Craig <lacraig3@gmail.com>
2025-01-22 13:17:03 -0500

    make qemu submodule

--- utils/panda_header/pandare_build.py
+++ utils/panda_header/pandare_build.py
@@ -5,8 +5,7 @@ import re
 import tree_sitter_c as tsc
 from tree_sitter import Language, Parser
 
-root = "../.."
-root = "/home/luke/workspace/qemu"
+root = "../../qemu"
 C_LANGUAGE = Language(tsc.language())
 parser = Parser(C_LANGUAGE)
 
