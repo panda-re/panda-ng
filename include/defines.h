d05fb8fe2e69308f80d0aac2c18f122b242cef38
Luke Craig <lacraig3@gmail.com>
2025-01-23 21:12:08 -0500

    add defines header

--- include/defines.h
+++ include/defines.h
@@ -1,1 +1,20 @@
 #define MEMTX_OK 0
+#define bswap32(x) ((unsigned int)__builtin_bswap32(x))
+#define bswap64(x) ((unsigned int)__builtin_bswap64(x))
+
+/**
+ * @brief Wrapper macro for quashing warnings for unused variables.
+ */
+#if defined(UNUSED)
+#elif defined(__GNUC__)
+#define UNUSED(x) x __attribute__((unused))
+#elif defined(__LCLINT__)
+#define UNUSED(x) /*@unused@*/ x
+#else
+#define UNUSED(x) x
+#endif
+
+#ifndef likely
+#define likely(x)   __builtin_expect(!!(x), 1)
+#define unlikely(x)   __builtin_expect(!!(x), 0)
+#endif
