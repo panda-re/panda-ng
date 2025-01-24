725e2513091bffafa9de18c8e0f37b4db99765e4
jamcleod <Jordan.McLeod@ll.mit.edu>
2025-01-23 21:52:15 -0500

    Simplify C skeleton plugin

--- plugins/cskeleton/cskeleton.c
+++ plugins/cskeleton/cskeleton.c
@@ -15,23 +15,18 @@ PANDAENDCOMMENT */
 // the PRIx64 macro
 #define __STDC_FORMAT_MACROS
 
-// #include "panda/plugin.h"
-
 #include "panda.h"
-
-
-bool init_plugin(void *);
-void uninit_plugin(void *);
-
-void before_block_exec(CPUState *env, TranslationBlock *tb);
 
 void before_block_exec(CPUState *env, TranslationBlock *tb) {
     printf("before_block_exec\n");
 }
 
 bool init_plugin(void *self) {
-    panda_cb pcb = { .before_block_exec = before_block_exec };
-    panda_register_callback(self, PANDA_CB_START_BLOCK_EXEC, pcb);
+    panda_register_callback(
+        self,
+        PANDA_CB_START_BLOCK_EXEC,
+        (panda_cb){ .before_block_exec = before_block_exec }
+    );
 
     return true;
 }
