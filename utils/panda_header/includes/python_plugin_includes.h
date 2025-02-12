e40d43baa6c8cd4f933a81416bddba328896a525
Luke Craig <lacraig3@gmail.com>
2025-02-11 19:13:18 -0500

    drop panda_header

--- utils/panda_header/includes/python_plugin_includes.h
+++
@@ -1,5 +0,0 @@
-#include "syscalls2/syscalls2_info.h"
-#include "syscalls2/syscalls_ext_typedefs.h"
-#include "osi/osi_types.h"
-#include "proc_start_linux/proc_start_linux.h"
-#include "hypercaller/hypercaller.h"
