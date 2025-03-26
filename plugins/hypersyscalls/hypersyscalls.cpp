df5f5f52ecf38c3ec398763d96c483fefbbe65d3
Luke Craig <lacraig3@gmail.com>
2025-03-26 16:57:44 -0400

    fixup hypersyscalls

--- plugins/hypersyscalls/hypersyscalls.cpp
+++ plugins/hypersyscalls/hypersyscalls.cpp
@@ -12,13 +12,17 @@ PANDAENDCOMMENT */
 #include <unordered_map>
 #include <vector>
 #include <string>
+#include <cstring>
+#include <cstdint>
+#include <iostream>
 #include <tuple>
-#include "hypersyscalls.h"
+#include <atomic>
 #include "json.hpp"
 
 using namespace std;
 
 extern "C" {
+    #include "hypersyscalls.h"
     bool init_plugin(void *);
     void uninit_plugin(void *);
     void hc_syscall_enter(CPUState *cpu);
@@ -26,31 +30,30 @@ extern "C" {
     void hc_setup_syscall(CPUState *cpu);
     #include <hypercaller/hypercaller.h>
 }
-
-struct syscall_info {
-    string name;
-    uint64_t syscall_nr;
-    vector<tuple<string, string>> args;
-};
-
 #ifdef DEBUG_HYPERSYSCALLS
-#define log(...)
-#else
 #define log(...) printf(__VA_ARGS__)
+#else
+#define log(...)
 #endif
 
-unordered_map<uint64_t, struct syscall_info> registered_syscalls;
-void (*hypercall_register_hypercall)(uint32_t magic, hypercall_t);
+// syscall_nr -> syscall_info
+unordered_map<uint64_t, struct syscall_prototype> syscall_info_table;
 
+// store the callbacks that are registered before the table is initialized
+vector<struct syscall_hook> preregistered_callbacks;
+
+atomic<ID> id_counter = 0;
+unordered_map<ID, struct syscall_hook> syscall_hooks;
+
+vector<ID> syscall_all_enter_cbs;
+vector<ID> syscall_all_return_cbs;
+
+unordered_map<uint64_t, vector<ID>> syscall_enter_cbs;
+unordered_map<uint64_t, vector<ID>> syscall_return_cbs;
+
+void (*hypercall_register_hypercall)(uint32_t magic, hypercall_t);
 bool table_initialized = false;
 
-struct igloo_sysret {
-	long nr;
-	int nargs;
-	long args[6];
-	// char strings[MAX_ARGS][PATH_MAX];
-	long* ret;
-} __packed;
 
 string read_str(CPUState* cpu, target_ulong ptr){
     string buf = "";
@@ -69,45 +72,136 @@ string read_str(CPUState* cpu, target_ulong ptr){
     return buf;
 }
 
+void do_register_syscall(struct syscall_hook *cb){
+    bool any_set = false;
+    cb->enabled = true;
+    int id;
+    syscall_hooks[id=++id_counter] = *cb;
+    if (cb->on_all){
+        if (cb->on_enter){
+            syscall_all_enter_cbs.push_back(id);
+            any_set = true;
+        }
+        if (cb->on_return){
+            syscall_all_return_cbs.push_back(id);
+            any_set = true;
+        }
+    }else {
+        for (auto &syscall : syscall_info_table){
+            if (strcmp(syscall.second.name, cb->name) == 0){
+                if (cb->on_enter){
+                    syscall_return_cbs[syscall.first].push_back(id);
+                    any_set = true;
+                }
+                if (cb->on_return){
+                    syscall_return_cbs[syscall.first].push_back(id);
+                    any_set = true;
+                }
+            }
+        }
+    }
+    if (!any_set){
+        printf("No syscall found with name %s\n", cb->name);
+    }
+}
+
+void register_syscall_cb(struct syscall_hook *cb){
+    if (table_initialized){
+        do_register_syscall(cb);
+    }else{
+        preregistered_callbacks.push_back(*cb);
+    }
+}
+
+void log_syscall(struct syscall_prototype *prototype, struct syscall *sysret){
+    log("%s %ld (", prototype->name, sysret->nr);
+    for (int i = 0; i < prototype->nargs; i++){
+        log("%s %s %" PRIx64 " , ", prototype->types[i],  prototype->names[i], sysret->args[i]);
+    }
+    log(")\n");
+}
+
+void loop_run_cbs(CPUState *cpu, vector<ID> &cbs, struct syscall_prototype* syscall_proto, 
+            struct syscall *sysret){
+    for (auto id : cbs){
+        auto h = syscall_hooks.find(id);
+        if (h == syscall_hooks.end()){
+            log("hook not found\n");
+            continue;
+        }
+        log("loop_run_cbs\n");
+        auto &hook = h->second;
+        if (!hook.enabled){
+            log("hook disabled\n");
+            continue;
+        }
+        log("running cb\n");
+        hook.cb(cpu, syscall_proto, sysret, &hook);
+    }
+}
+
+void hc_syscall(CPUState *cpu, unordered_map<uint64_t, vector<ID>> &cb_map, vector<ID> &all_cbs){
+    if (!table_initialized){
+        printf("table not initialized\n");
+        return;
+    }
+    int reg1 = panda_get_syscall_arg(cpu, 1);
+    
+    struct syscall sysret;
+    log("reg1: %x\n", reg1);
+    if (panda_virtual_memory_read(cpu, reg1, (uint8_t*)&sysret, sizeof(struct syscall)) != MEMTX_OK){
+        printf("failed to read sysret\n");
+        panda_set_retval(cpu, (target_ulong) 0);
+        return;
+    }
+    if (sysret.known_magic != SYSCALL_HC_KNOWN_MAGIC){
+        log("FATAL: struct syscall out of sync with plugin!!\n");
+        return;
+    }
+
+    auto syscall_proto_search = syscall_info_table.find(sysret.nr);
+    if (syscall_proto_search == syscall_info_table.end()){
+        log("syscall %ld not registered\n", sysret.nr);
+        return;
+    }
+    auto syscall_proto = syscall_proto_search->second;
+
+    log_syscall(&syscall_proto, &sysret);
+
+    struct syscall clean_copy;
+    memcpy(&clean_copy, &sysret, sizeof(struct syscall));
+    
+    loop_run_cbs(cpu, all_cbs, &syscall_proto, &sysret);
+
+    auto cbs = cb_map.find(sysret.nr);
+    if (cbs != cb_map.end()){
+        loop_run_cbs(cpu, cbs->second, &syscall_proto, &sysret);
+    }
+
+    if (memcmp(&clean_copy, &sysret, sizeof(struct syscall)) != 0){
+        log("syscall modified\n");
+        panda_virtual_memory_write(cpu, reg1, (uint8_t*)&sysret, sizeof(struct syscall));
+    }
+    panda_set_retval(cpu, 0);
+}
+
+/**
+ * This function is called when a syscall is entered
+ */
 void hc_syscall_enter(CPUState *cpu){
-    struct igloo_sysret sysret;
-    int reg1 = panda_get_syscall_arg(cpu, 1);
-    if (panda_virtual_memory_read(cpu, reg1, (uint8_t*)&sysret, sizeof(sysret)) != MEMTX_OK){
-        printf("failed to read sysret\n");
-    }
-
-    auto syscall = registered_syscalls.find(sysret.nr);
-    if (syscall == registered_syscalls.end()){
-        printf("syscall %ld not registered\n", sysret.nr);
-        return;
-    }
-    printf("enter %s %ld (", syscall->second.name.c_str(), sysret.nr);
-    for (int i = 0; i < syscall->second.args.size(); i++){
-        printf("%s %s %ld %x, ", std::get<0>(syscall->second.args[i]).c_str(),  std::get<1>(syscall->second.args[i]).c_str(), sysret.args[i]);
-    }
-    printf(")\n");
-
+    hc_syscall(cpu, syscall_enter_cbs, syscall_all_enter_cbs);
 }
 
+/**
+ * This function is called when a syscall is returning
+ */
 void hc_syscall_return(CPUState *cpu){
-    struct igloo_sysret sysret;
-    int reg1 = panda_get_syscall_arg(cpu, 1);
-    if (panda_virtual_memory_read(cpu, reg1, (uint8_t*)&sysret, sizeof(sysret)) != MEMTX_OK){
-        printf("failed to read sysret\n");
-    }
-
-    auto syscall = registered_syscalls.find(sysret.nr);
-    if (syscall == registered_syscalls.end()){
-        printf("syscall %ld not registered\n", sysret.nr);
-        return;
-    }
-    printf("ret %s %ld (", syscall->second.name.c_str(), sysret.nr);
-    for (int i = 0; i < syscall->second.args.size(); i++){
-        printf("%s %s %ld %x, ", std::get<0>(syscall->second.args[i]).c_str(),  std::get<1>(syscall->second.args[i]).c_str(), sysret.args[i]);
-    }
-    printf(")\n");
+    hc_syscall(cpu, syscall_return_cbs, syscall_all_return_cbs);
 }
 
+/**
+ * This function receives a definition of a syscall in JSON format from the guest via hypercall
+ */
 void hc_setup_syscall(CPUState *cpu){
     int reg1 = panda_get_syscall_arg(cpu, 1);
 
@@ -119,20 +213,26 @@ void hc_setup_syscall(CPUState *cpu){
     if (reg1 == 0){
         log("table initialized\n");
         table_initialized = true;
+        for (auto &cb : preregistered_callbacks){
+            do_register_syscall(&cb);
+        }
         return;
     }
     string buf = read_str(cpu, reg1);
     auto j = nlohmann::json::parse(buf);
-    struct syscall_info sysinfo;
-    sysinfo.name = j["name"];
+    struct syscall_prototype sysinfo;
+    sysinfo.name = strdup(j["name"].get<string>().c_str());
     sysinfo.syscall_nr = j["syscall_nr"];
-    log("%s %ld (", sysinfo.name.c_str(), sysinfo.syscall_nr);
+    log("%s %ld (", sysinfo.name, sysinfo.syscall_nr);
+    int i = 0;
     for (auto& arg : j["args"]){
-        sysinfo.args.push_back(std::make_tuple(arg[0], arg[1]));
-        log("%s %s, ", arg[0].get<string>().c_str(), arg[1].get<string>().c_str());
+        sysinfo.types[i] = strdup(arg[0].get<string>().c_str());
+        sysinfo.names[i] = strdup(arg[1].get<string>().c_str());
+        i++;
     }
+    sysinfo.nargs = i;
     log(")\n");
-    registered_syscalls[sysinfo.syscall_nr] = sysinfo;
+    syscall_info_table[sysinfo.syscall_nr] = sysinfo;
 }
 
 bool init_plugin(void *self) {
--- plugins/hypersyscalls/hypersyscalls.h
+++ plugins/hypersyscalls/hypersyscalls.h
@@ -5,13 +5,63 @@
 #define IGLOO_HYP_SYSCALL_ENTER 0x1338
 #define IGLOO_HYP_SYSCALL_RETURN 0x1339
 
+
 // BEGIN_PYPANDA_NEEDS_THIS -- do not delete this comment bc pypanda
 // api autogen needs it.  And don't put any compiler directives
 // between this and END_PYPANDA_NEEDS_THIS except includes of other
 // files in this directory that contain subsections like this one.
 
+/**
+ * This stores information about a current syscall
+ */
+
+#define MAX_ARGS 7
+
+// add 1 if struct syscall changes
+#define SYSCALL_HC_KNOWN_MAGIC 0x1234
+struct syscall {
+	uint64_t known_magic;
+	uint64_t nr;
+	uint64_t args[MAX_ARGS];
+	uint64_t pc;
+	int64_t retval;
+	uint64_t skip_syscall;
+	uint64_t task;
+};
+
+/**
+ * This stores information about the prototype of a syscall
+ */
+struct syscall_prototype {
+    char *name;
+    uint64_t syscall_nr;
+    int nargs;
+    const char* types[MAX_ARGS];
+    const char* names[MAX_ARGS];
+};
 
 
+struct syscall_hook;
+typedef void (*syscall_cb_t)(CPUState *cpu, const struct syscall_prototype *syscall,
+                             const struct syscall *sysret, struct syscall_hook *hook);
+
+typedef uint32_t ID;
+struct syscall_hook {
+    ID id;              // Unique id
+    bool on_enter;      // true if hook should trigger on entry
+    bool on_return;     // true if hook should trigger on return
+    bool on_all;        // true if hook should trigger on all syscalls
+    char name[128];     // name must fully match
+    bool enabled;       // true if hook is enabled
+    syscall_cb_t cb;    // callback function
+};
+
+enum sysret_type {
+    RETURN_OK = 0,
+    RETURN_SKIP = 1, // skip the syscall on enter
+};
+
+void register_syscall_cb(struct syscall_hook *hook);
 
 // END_PYPANDA_NEEDS_THIS -- do not delete this comment!
 
