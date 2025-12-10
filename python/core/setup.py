1a2c9ae06331a776c8d936c969ddd25dd0042d21
aking <aking@mit.edu>
2025-12-10 14:01:06 -0500

    setup: fix setup.py issues with pip environment
    setup depended on local packages, and executed code using them
    outside of the setup stages, which breaks pip installation.
    
    pandre2 should now be installable from pip

--- python/core/setup.py
+++ python/core/setup.py
@@ -1,29 +1,42 @@
-from setuptools import setup
+import setuptools
 import os, re
 from os.path import realpath, join, exists
-from pandare_build import handle_python
 from glob import glob
+import distutils.command.build
 
 root = realpath("../..")
 
-def run_install():
-    local_panda_ng = realpath(join(root, "local_packages/panda-ng"))
-    if exists(local_panda_ng):
-        panda_ng_path = local_panda_ng
-    elif exists("/usr/include/panda-ng"):
-        panda_ng_path = "/usr/include/panda-ng"
-    else:
-        raise Exception("Could not find panda-ng path. Considered /usr/include/panda-ng and local_packages/panda-ng")
-    paths = glob(join(panda_ng_path, "panda_python_*.h"))
-    for path in paths:
-        arch = re.search("panda_python_(.*).h", path).group(1)
-        print(f"Building auto-generated python bindings for {arch}")
-        handle_python(arch, open(path).read())
+class build_python_autogen(setuptools.Command):
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        import pandare_build
+        local_panda_ng = realpath(join(root, "local_packages/panda-ng"))
+        if exists(local_panda_ng):
+            panda_ng_path = local_panda_ng
+        elif exists("/usr/include/panda-ng"):
+            panda_ng_path = "/usr/include/panda-ng"
+        else:
+            raise Exception("Could not find panda-ng path. Considered /usr/include/panda-ng and local_packages/panda-ng")
+        paths = glob(join(panda_ng_path, "panda_python_*.h"))
+        plugin_dir = realpath(join(root, "plugins"))
+        if not exists(plugin_dir):
+            raise Exception("Could not find plugin directory. This should be impossible. Build environment issue. {}".format(repr(plugin_dir)))
+        for path in paths:
+            arch = re.search("panda_python_(.*).h", path).group(1)
+            print(f"Building auto-generated python bindings for {arch}")
+            pandare_build.handle_python(arch, open(path).read(), plugin_dir)
 
-run_install()
+class build(distutils.command.build.build):
+    _sub_command = ('build_python_autogen', None, )
+    _sub_commands = distutils.command.build.build.sub_commands
+    sub_commands = [_sub_command] + _sub_commands
+
 if os.path.exists("pandare2/version.txt"):
     with open("pandare2/version.txt") as f:
         version = f.read().strip()
 else:
     version = "0.0.1"
-setup(version=version)
+setuptools.setup(version=version, cmdclass=dict(build_python_autogen=build_python_autogen, build=build))
